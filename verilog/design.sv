1d6d01cb648ac4e877a55f97c68e56850fcea38c
Magi <paoloimbrmagi@gmail.com>
2024-06-03 19:45:15 +0200

    Last Added | Completare Relazione

--- verilog/design.sv
+++ verilog/design.sv
@@ -39,7 +39,7 @@ module FSMD(
     end
 
 //ATTENZIONE!! QUESTA FSM HA QUALCHE MODIFICA RISPETTO AL GRAFICO CHE SERVE
-  always @(state, control, manche, obblig) begin : FSM
+  always @(state, inizia, control, manche, obblig) begin : FSM
     case(state)
       //parto da start//
       3'b000:
@@ -47,38 +47,38 @@ module FSMD(
       	  next_state = state;
         end else begin
           partita = 2'b00;
-          next_state = 3'b001;
+          next_state = 3'b001; // NO ADV 
         end
       //parto da no adv//
       3'b001:
-        if(control == 1'b0 && (manche == 2'b00 || manche == 2'b11)) begin
+        if(~inizia && control == 1'b0 && (manche == 2'b00 || manche == 2'b11)) begin
           partita = 2'b00;
           next_state = state;
-        end else if(control == 1'b0 && manche==2'b01) begin
+        end else if(~inizia && control == 1'b0 && manche==2'b01) begin
           partita = 2'b00;
           next_state = 3'b010;
-        end else if(control == 1'b0 && manche==2'b10) begin
+        end else if(~inizia && control == 1'b0 && manche==2'b10) begin
           partita = 2'b00;
           next_state = 3'b011;
-        end else if(control == 1'b1 && inizia == 1'b0) begin
-          partita = 2'b11;
+        end else if(control == 1'b1 || inizia == 1'b1) begin
+          partita = 2'b11; 
           next_state = startend;
       	end
       //vantaggio g1//
       3'b010:
-        if(control == 1'b0 && (manche==2'b00 || manche == 2'b11)) begin
+        if(~inizia && control == 1'b0 && (manche==2'b00 || manche == 2'b11)) begin
           partita = 2'b00;
           next_state = 3'b010;
-        end else if(control == 1'b0 && manche==2'b01 && obblig) begin
+        end else if(~inizia && control == 1'b0 && manche==2'b01 && obblig) begin
           partita = 2'b00;
           next_state = 3'b100;
-        end else if(control == 1'b0 && manche==2'b10) begin
+        end else if(~inizia && control == 1'b0 && manche==2'b10) begin
           partita = 2'b00;
           next_state = 3'b001;
-        end else if(manche == 2'b01 && ~obblig) begin
+        end else if(~inizia && manche == 2'b01 && ~obblig) begin
           partita = 2'b01;
           next_state = startend;
-        end else if(control == 1'b1) begin
+        end else if(control == 1'b1 || inizia) begin
           partita = 2'b11;
           next_state = startend;
         end
@@ -86,86 +86,89 @@ module FSMD(
        //vantaggio g2//
       	// Controllo = 1 se le partite sono 0
        3'b011:
-         if(control == 1'b0 && (manche==2'b00 || manche == 2'b11)) begin
+         if(~inizia && control == 1'b0 && (manche==2'b00 || manche == 2'b11)) begin
            partita = 2'b00;
            next_state = state;
-         end else if (control == 1'b0 && manche==2'b01) begin
+         end else if (~inizia && control == 1'b0 && manche==2'b01) begin
            partita = 2'b00;
            next_state = 3'b001;
-         end else if (control == 1'b0 && manche==2'b10 && obblig) begin
+         end else if (~inizia && control == 1'b0 && manche==2'b10 && obblig) begin
            partita = 2'b00;
            next_state = 3'b101;
-         end else if(manche == 2'b10 && ~obblig) begin
+         end else if(~inizia && manche == 2'b10 && ~obblig) begin
           partita = 2'b10; 
            next_state = startend; // WIN
-        end else if(control == 1'b1) begin
+         end else if(control == 1'b1 || inizia) begin
            partita = 2'b11;
           next_state = startend;
         end
         //vantaggio g1+1//
         3'b100:
-          if(control == 1'b0 && (manche==2'b00 || manche == 2'b11)) begin
+          if(~inizia && control == 1'b0 && (manche==2'b00 || manche == 2'b11)) begin
             partita = 2'b00;
             next_state = state;
-          end else if (control == 1'b0 && manche==2'b01) begin
+          end else if (~inizia && control == 1'b0 && manche==2'b01) begin
             partita = 2'b00;
             next_state = 3'b110;
-          end else if (control == 1'b0 && manche==2'b10) begin
+          end else if (~inizia && control == 1'b0 && manche==2'b10) begin
             partita = 2'b00;
             next_state = 3'b010;
-          end else if(manche == 2'b01 && ~obblig) begin
+          end else if(~inizia && manche == 2'b01 && ~obblig) begin
             partita = 2'b01;
             next_state = startend;
-          end else if(control == 1'b1) begin
+          end else if(control == 1'b1 || inizia) begin
             partita = 2'b01;
             next_state = startend;
           end
       //vantaggio g2+1//
       3'b101:
-        if(control == 1'b0 && (manche==2'b00 || manche == 2'b11)) begin
+        if(~inizia && control == 1'b0 && (manche==2'b00 || manche == 2'b11)) begin
              partita = 2'b00;
              next_state = state;
-        end else if (control == 1'b0 && manche==2'b01) begin
+        end else if (~inizia && control == 1'b0 && manche==2'b01) begin
              partita = 2'b00;
              next_state = 3'b011;
-        end else if (control == 1'b0 && manche==2'b10) begin
+        end else if (~inizia && control == 1'b0 && manche==2'b10) begin
              partita = 2'b00;
              next_state = 3'b111;
-        end else if(manche == 2'b10 && ~obblig) begin
+        end else if(~inizia && manche == 2'b10 && ~obblig) begin
             partita = 2'b10;
           next_state = startend;
-      	  end else if(control == 1'b1) begin
+        end else if(control == 1'b1 || inizia) begin
             partita = 2'b10;
             next_state = startend;
           end
       //vantaggio g1+2//
         3'b110:
-          if(control == 1'b0 && manche==2'b00) begin
+          if(~inizia && control == 1'b0 && manche==2'b00) begin
               partita = 2'b00;
               next_state = state;
-          end else if ((control == 1'b0 && (manche==2'b10 || manche == 2'b11 || manche == 2'b01)) || control == 1'b1) begin
+          end else if ((control == 1'b0 && (manche==2'b10 || manche == 2'b11 || manche == 2'b01)) || control == 1'b1 || inizia) begin
               partita = 2'b01;
             next_state = startend;
             end
         //vantaggio g2+2//
         3'b111:
-          if(control == 1'b0 && manche==2'b00) begin
+          if(~inizia && control == 1'b0 && manche==2'b00) begin
               partita = 2'b00;
-              next_state = 3'b111;
-          end else if ((control == 1'b0 && (manche==2'b10 || manche == 2'b11 || manche == 2'b01)) || control == 1'b1) begin
+              next_state = startend;
+          end else if ((control == 1'b0 && (manche==2'b10 || manche == 2'b11 || manche == 2'b01)) || control == 1'b1 || inizia) begin
               partita = 2'b10;
             	next_state = startend;
           end
     endcase
   end // END FSM
 
+  
 always @(posedge clk) begin: DATAPATH
 		
    
     if(inizia) begin // QUANDO INIZIA = 1     
-      
+		
+      // Inizializzo le manche
       manche = 2'b00;
       partita = 2'b00;
+      
       mv1=2'b00;      mv2=2'b00;
       // primo = 10 -> 01000      secondo = 11 -> 00011
       // primo + secondo 01011 + 00100
--- verilog/testbench.sv
+++ verilog/testbench.sv
@@ -28,9 +28,9 @@ module tb_FSMD();
         tbf = $fopen("testbench.script", "w");
         outf = $fopen("output_verilog.txt", "w");
         $fdisplay(tbf, "read_blif FSMD.blif");
-
-        #10
-
+	
+      	#30 // Aspettare un po' di più
+        
       	inizia = 1'b1;
         primo = 2'b00;
         secondo = 2'b11;
@@ -181,6 +181,39 @@ module tb_FSMD();
         #18
       	
     	inizia = 1'b1;
+      	primo = 2'b00;
+      	secondo = 2'b00;
+      
+      	#2 
+
+        $fdisplay(tbf, "simulate %b %b %b %b %b", inizia, primo[1], primo[0], secondo[1], secondo[0]);
+        $fdisplay(outf, "Outputs: %b %b %b %b", manche[1], manche[0], partita[1], partita[0]);
+      
+      	#18
+      	
+      	inizia = 1'b0;
+      	primo = 2'b01;
+      	secondo = 2'b11;
+      	
+      	#2 
+
+        $fdisplay(tbf, "simulate %b %b %b %b %b", inizia, primo[1], primo[0], secondo[1], secondo[0]);
+        $fdisplay(outf, "Outputs: %b %b %b %b", manche[1], manche[0], partita[1], partita[0]);
+      
+      
+      	inizia = 1'b1;
+      	primo = 2'b00;
+      	secondo = 2'b00;
+      	
+      
+      #20 // Aspettare qualche nanosecondo in più a causa di
+      // INIZIA = 1
+
+        $fdisplay(tbf, "simulate %b %b %b %b %b", inizia, primo[1], primo[0], secondo[1], secondo[0]);
+        $fdisplay(outf, "Outputs: %b %b %b %b", manche[1], manche[0], partita[1], partita[0]);
+      
+      
+      	// FINE
         $fdisplay(tbf, "quit");
 
         $fclose(tbf);
